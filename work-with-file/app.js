0dcdad0569daca77b2cf5f67275d44ef70c529fe
Oleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2024-07-04 19:11:39 +0300

    test

--- work-with-file/app.js
+++ work-with-file/app.js
@@ -1,6 +1,34 @@
-const fs  = require("fs");
+const fs  = require("fs/promises");
 
-fs.readFile("./files/file.txt",(err, data) => {
-    console.log(err);
-    console.log(data);
-});
+// fs.readFile("./files/file.txt",(err, data) => {
+//     console.log(err);
+//     console.log(data);
+// });
+
+const fileOperation = async({filePath, action, data}) => {
+    switch(action){
+        case "read":
+
+            // const data = await fs.readFile(filePath);
+            // const text = data.toString();
+            // console.log(text);
+
+            // Декодуємо текст  
+            const text = await fs.readFile(filePath, "utf-8");
+            console.log(text);
+            break;    
+
+        case "add":
+            await fs.appendFile(filePath, data);
+            break;  
+        case "replace":
+            const result = await fs.writeFile(filePath, data);
+            console.log(result);
+            break;
+                   
+    }
+}
+
+// fileOperation({filePath: "./files/file.txt", action: "read"})
+// fileOperation({filePath: "./files/file.txt", action: "add", data: "\ntest append file"});
+fileOperation({filePath: "./files/file.txt", action: "replace", data: "test replece file"});
